9996ee5223e9417d8ccd2a171dfb93a24798c7ed
Tammy Cravit <tammycravit@me.com>
2019-08-23 14:48:48 -0700

    Removed library modules

--- library/tc_hacks.h
+++
@@ -1,6 +0,0 @@
-/* Helpers from https://electronics.stackexchange.com/questions/80613/bit-type-for-avr-microcontroller-programming */
-
-#define BIT_GET(p,m) ((p)&(m))
-#define BIT_SET(p,m) ((p)|=(m))
-#define BIT_CLEAR(p,m) ((p)&=~(m))
-#define BIT(x) (0x01<<(x))
